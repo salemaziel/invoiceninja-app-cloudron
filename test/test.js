2a1eb6361a489a51c72e5c6805c9a7361fd8802d
Johannes Zellner <johannes@nebulon.de>
2021-03-23 11:09:54 +0100

    Add more test and especially pdf rendering

--- test/test.js
+++ test/test.js
@@ -31,8 +31,9 @@ describe('Application life cycle test', function () {
     const password = 'changeme';
 
     const VENDOR_NAME = 'Cloudron Vendor';
+    const CLIENT_NAME = 'Cloudron Client';
 
-    var app, token, vendorId;
+    var app, token, clientId, vendorId, invoiceId;
 
     function sleep(millis) {
         return new Promise(resolve => setTimeout(resolve, millis));
@@ -76,18 +77,91 @@ describe('Application life cycle test', function () {
         });
     }
 
+    function createClient(callback) {
+        superagent.post('https://' + app.fqdn + '/api/v1/clients').send({ name: CLIENT_NAME }).set('X-API-Token', token ).end(function (error, result) {
+            expect(error).to.be(null);
+            expect(result.status).to.eql(200);
+
+            clientId = result.body.data.id;
+
+            callback();
+        });
+    }
+
+    function getClient(callback) {
+        superagent.get('https://' + app.fqdn + '/api/v1/clients/' + clientId).set('X-API-Token', token ).end(function (error, result) {
+            expect(error).to.be(null);
+            expect(result.status).to.eql(200);
+            expect(result.body.data.name).to.equal(CLIENT_NAME);
+
+            callback();
+        });
+    }
+
+
+    function createInvoice(callback) {
+        superagent.get('https://' + app.fqdn + '/api/v1/invoices/create').set('X-API-Token', token ).end(function (error, result) {
+            expect(error).to.be(null);
+            expect(result.status).to.eql(200);
+
+            var invoice = result.body;
+            invoice.client_id = clientId;
+
+            superagent.post('https://' + app.fqdn + '/api/v1/invoices').send(invoice).set('X-API-Token', token ).end(function (error, result) {
+                expect(error).to.be(null);
+                expect(result.status).to.eql(200);
+
+                invoiceId = result.body.data.id;
+
+                callback();
+            });
+        });
+    }
+
+    function getInvoice(callback) {
+        superagent.get('https://' + app.fqdn + '/api/v1/invoices/' + invoiceId).set('X-API-Token', token ).end(function (error, result) {
+            expect(error).to.be(null);
+            expect(result.status).to.eql(200);
+            expect(result.body.data.client_id).to.equal(clientId);
+
+            callback();
+        });
+    }
+
+
+    function getPreviewPdf(callback) {
+        // data taken from a UI request dump
+        var designData = {"entity_type":"invoice","entity_id":"","design":{"name":"","design":{"includes":"<style id=\"style\">\n    :root {\n        --primary-color: $primary_color;\n        --secondary-color: $secondary_color;\n    }\n\n    body {\n        -webkit-font-smoothing: antialiased;\n        -moz-osx-font-smoothing: grayscale;\n        font-family: Arial, Helvetica, sans-serif;\n        font-size: \"$font_size\";\n        zoom: 80%;\n    }\n\n    @page {\n        margin: $global_margin;\n    }\n\n    p {\n        margin: 0;\n        padding: 0;\n    }\n\n    .header-container {\n        display: grid;\n        grid-template-columns: 1fr 1fr 1fr;\n        gap: 20px;\n    }\n\n    .header-container .company-logo {\n        height: 4rem;\n    }\n\n    #company-details {\n        display: flex;\n        flex-direction: column;\n    }\n\n    #company-details > span:first-child {\n        color: var(--primary-color);\n    }\n\n    #company-address {\n        display: flex;\n        flex-direction: column;\n    }\n\n    .entity-label {\n        text-transform: uppercase;\n        margin-top: 3.5rem;\n        padding-left: 1rem;\n        margin-bottom: 1rem;\n        font-weight: bold;\n        color: var(--primary-color);\n    }\n\n    .client-and-entity-wrapper {\n        display: grid;\n        grid-template-columns: 1fr 1fr;\n        padding: 1rem;\n        border-top: 1px solid #d8d8d8;\n        border-bottom: 1px solid #d8d8d8;\n    }\n\n    #entity-details {\n        text-align: left;\n    }\n\n    #entity-details > tr,\n    #entity-details th {\n        font-weight: normal;\n    }\n\n    #client-details {\n        display: flex;\n        flex-direction: column;\n    }\n\n    #client-details > :first-child {\n        font-weight: bold;\n    }\n\n    #product-table,\n    #delivery-note-table,\n    #task-table {\n        margin-top: 3rem;\n        /* margin-bottom: 200px; */\n        min-width: 100%;\n        table-layout: fixed;\n        overflow-wrap: break-word;\n    }\n\n    .task-time-details {\n        display: block;\n        margin-top: 5px;\n        color: grey;\n    }\n\n    #product-table > thead,\n    #delivery-note-table > thead,\n    #task-table > thead {\n        text-align: left;\n    }\n\n    #product-table > thead > tr > th,\n    #delivery-note-table > thead > tr > th,\n    #task-table > thead > tr > th {\n        font-size: 1.1rem;\n        padding-bottom: 1.5rem;\n        padding-left: 1rem;\n    }\n\n    #product-table > thead > tr > th:nth-last-child(1),\n    #delivery-note-table > thead > tr > th:nth-last-child(1),\n    #task-table > thead > tr > th:nth-last-child(1) {\n        text-align: right;\n    }\n\n    #product-table > tbody > tr > td,\n    #delivery-note-table > tbody > tr > td,\n    #task-table > tbody > tr > td {\n        border-top: 1px solid #d8d8d8;\n        border-bottom: 1px solid #d8d8d8;\n        padding: 1rem;\n    }\n\n    #product-table > tbody > tr > td:first-child,\n    #delivery-note-table > tbody > tr > td:first-child,\n    #task-table > tbody > tr > td:first-child {\n        color: var(--primary-color);\n    }\n\n    #product-table > tbody > tr > td:last-child,\n    #delivery-note-table > tbody > tr > td:last-child,\n    #task-table > tbody > tr > td:last-child {\n        text-align: right;\n    }\n\n    #product-table > tbody > tr:nth-child(odd),\n    #delivery-note-table > tbody > tr:nth-child(odd),\n    #task-table > tbody > tr:nth-child(odd) {\n        background-color: #f5f5f5;\n    }\n\n    #table-totals {\n        page-break-inside: avoid;\n    }\n\n    #table-totals {\n        display: grid;\n        grid-template-columns: 2fr 1fr;\n        padding-top: .5rem;\n        gap: 80px;\n    }\n\n    #table-totals .totals-table-right-side>* {\n        display: grid;\n        grid-template-columns: 1fr 1fr;\n    }\n\n    #table-totals>.totals-table-right-side>*> :nth-child(1) {\n        text-align: left;\n    }\n\n    #table-totals>.totals-table-right-side>*> :nth-child(2) {\n        text-align: right;\n    }\n\n    #table-totals\n    > *\n    [data-element='product-table-balance-due-label'],\n    #table-totals\n    > *\n    [data-element='product-table-balance-due'] {\n        font-weight: bold;\n    }\n\n    #table-totals\n    > *\n    [data-element='product-table-balance-due'] {\n        color: var(--primary-color);\n    }\n\n    #table-totals > * > :last-child {\n        text-align: right;\n        padding-right: 1rem;\n    }\n</style>\n","header":"<div id=\"header\"></div>\n","body":"<div id=\"body\">\n    <div class=\"header-container\">\n        <img class=\"company-logo\" src=\"$company.logo\" alt=\"$company.name logo\">\n\n        <div id=\"company-details\"></div>\n        <div id=\"company-address\"></div>\n    </div>\n\n    <p class=\"entity-label\">$entity_label</p>\n    <div class=\"client-and-entity-wrapper\">\n        <table id=\"entity-details\" cellspacing=\"0\"></table>\n\n        <div id=\"client-details\"></div>\n    </div>\n\n    <table id=\"product-table\" cellspacing=\"0\"></table>\n\n    <table id=\"task-table\" cellspacing=\"0\"></table>\n\n    <div id=\"table-totals\" cellspacing=\"0\"></div>\n\n    <table id=\"delivery-note-table\" cellspacing=\"0\"></table>\n</div>\n","product":"","task":"","footer":"<div id=\"footer\"></div>\n"},"is_custom":true,"created_at":0,"updated_at":0,"archived_at":0,"id":"-113","isChanged":false,"is_deleted":false}};
+
+        superagent.post('https://' + app.fqdn + '/api/v1/preview').send(designData).set('X-API-Token', token ).end(function (error, result) {
+            expect(error).to.be(null);
+            expect(result.status).to.eql(200);
+            expect(result.type).to.eql('application/pdf');
+
+            callback();
+        });
+    }
+
     xit('build app', function () { execSync('cloudron build', EXEC_ARGS); });
     it('install app', function () { execSync('cloudron install --location ' + LOCATION, EXEC_ARGS); });
 
     it('can get app information', getAppInfo);
 
     it('can login', login);
-    it('can create a project', createVendor);
-    it('project exists', getVendor);
+    it('can create a vendor', createVendor);
+    it('vendor exists', getVendor);
+    it('can create a client', createClient);
+    it('client exists', getClient);
+    it('can create an invoice', createInvoice);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('can restart app', function () { execSync('cloudron restart --app ' + app.id, EXEC_ARGS); });
 
-    it('project exists', getVendor);
+    it('vendor exists', getVendor);
+    it('client exists', getClient);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('backup app', function () { execSync('cloudron backup create --app ' + app.id, EXEC_ARGS); });
 
@@ -99,12 +173,18 @@ describe('Application life cycle test', function () {
         execSync(`cloudron restore --backup ${backups[0].id} --app ${app.id}`, EXEC_ARGS);
     });
 
-    it('project exists', getVendor);
+    it('vendor exists', getVendor);
+    it('client exists', getClient);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('move to different location', function () { execSync('cloudron configure --location ' + LOCATION + '2 --app ' + app.id, EXEC_ARGS); });
     it('can get new app information', getAppInfo);
 
-    it('project exists', getVendor);
+    it('vendor exists', getVendor);
+    it('client exists', getClient);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('uninstall app', function () { execSync('cloudron uninstall --app ' + app.id, EXEC_ARGS); });
 
@@ -113,13 +193,21 @@ describe('Application life cycle test', function () {
     it('can get app information', getAppInfo);
 
     it('can login', login);
-    it('can create a project', createVendor);
-    it('project exists', getVendor);
+    it('can create a vendor', createVendor);
+    it('vendor exists', getVendor);
+    it('can create a client', createClient);
+    it('client exists', getClient);
+    it('can create an invoice', createInvoice);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('can update', function () { execSync('cloudron update --app ' + app.id, EXEC_ARGS); });
 
     it('can login', login);
-    it('project exists', getVendor);
+    it('vendor exists', getVendor);
+    it('client exists', getClient);
+    it('invoice exists', getInvoice);
+    it('can render preview pdf', getPreviewPdf);
 
     it('uninstall app', function () { execSync('cloudron uninstall --app ' + app.id, EXEC_ARGS); });
 });
